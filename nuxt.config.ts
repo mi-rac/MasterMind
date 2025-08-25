45b58a45702ee1428bc4acc1ca5caaf07dd30d0d
Mihai Racoreanu <mihai@bodyswaps.co>
2025-08-25 23:30:59 +0100

    fetched localisations

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,14 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/ui-pro'],
+  modules: ['@nuxt/eslint', '@nuxt/ui-pro', '@nuxtjs/i18n', '@nuxt/icon'],
+  i18n: {
+    defaultLocale: 'en',
+    locales: [
+      { code: 'en', file: 'locales/en.json' },
+      { code: 'ro', file: 'locales/ro.json' }
+    ]
+  },
   ui: {
     theme: {
       colors: [
